f0150deff22ba661fc86fb52981c93125868e673
AshwinJ21 <69934358+AshwinJ21@users.noreply.github.com>
2020-08-31 11:25:09 -0500

    Update Jeroo.java

--- Jeroo.java
+++ Jeroo.java
@@ -6,7 +6,60 @@ public class Jeroo extends JerooBase {
 
     // Put your own methods here    
     
+    public void hopPlant(){
+      hop();
+      plant();
+    }
+
+    public void hopHopPlant(){
+      hop();
+      hopPlant();
+    }
     
+    public void turnAround(){
+      turn(LEFT);
+      turn(LEFT);
+    }
+    
+    public void newLineRight1(){
+      hop();
+      turn(RIGHT);
+      hopPlant();
+    }
+
+    public void newLineRight2(){
+      turn(LEFT);
+      hopHopPlant();
+    }
+
+    public void newLineRight(){
+      newLineRight1();
+      newLineRight2();
+    }
+
+    public void newLineLeft(){
+      hop();
+      turn(LEFT);
+      hopPlant();
+      turn(RIGHT);
+    }
+
+    public void endLine(){
+      hopHopPlant();
+      hopHopPlant();
+      turnAround();
+    }
+
+    public void setupBowling(){
+      newLineRight();
+      endLine();
+      newLineLeft();
+      endLine();
+      newLineRight();
+      turnAround();
+      newLineLeft();
+      hop();
+    }
 
 
     
